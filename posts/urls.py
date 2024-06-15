6cfb7ad15e72c98549635d616a8176bcb40523df
Hillary Vulimu <59297612+hillaryvulimu@users.noreply.github.com>
2024-06-15 15:12:06 +0300

    added search feature

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import SimpleRouter
 
 from .viewsets import PostViewSet, UserViewSet
 
-from .views import CategoryListAPIView, CategoryDetailAPIView
+from .views import CategoryListAPIView, CategoryDetailAPIView, PostSearchAPIView
 
 router = SimpleRouter()
 router.register("users", UserViewSet, basename="users")
@@ -13,5 +13,6 @@ router.register("", PostViewSet, basename="posts")
 urlpatterns = [
     path('categories/', CategoryListAPIView.as_view(), name='categories'),
     path('categories/<str:category>/', CategoryDetailAPIView.as_view(), name='category-detail'),
+    path('search/', PostSearchAPIView.as_view(), name='search'),
     path('', include(router.urls)),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
 
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.db.models import Q
 
 from .serializers import CategorySerializer, PostSerializer
 
 from .models import Post
 
 
+# Posts categories
 class CategoryListAPIView(ListAPIView):
     serializer_class = CategorySerializer
 
@@ -24,3 +29,24 @@ class CategoryDetailAPIView(ListAPIView):
     def get_queryset(self):
         category = self.kwargs['category']
         return Post.objects.filter(category=category)
+
+
+# Search
+class PostSearchAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        query = request.query_params.get('q', None)
+
+        if query:
+            posts = Post.objects.filter(
+                Q(title__icontains=query) | 
+                Q(body__icontains=query) |
+                Q(category__icontains=query)
+                # | Q(author__first_name__icontains=query)  # Search by author's first name
+                # | Q(author__last_name__icontains=query)    # Search by author's last name   
+            )
+        else:
+            posts = Post.objects.all()
+            # posts = { 'error': 'No results matching your query.'}
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
